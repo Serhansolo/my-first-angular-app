ba902eb78eda5bb2699e02ee86448d748de60638
Serhan Uygur <serhan.uygur@gmail.com>
2017-09-05 09:43:25 +0200

    Added some animation thigns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import {Component} from "@angular/core";
 import {DataService} from "./data.service";
+import {trigger, state, style, transition, animate, keyframes} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('animateSize', [
+      state('small', style({
+        transform: 'scale(1)'
+      })),
+      state('large', style({
+        transform: 'scale(1.05)'
+      })),
+      transition('small <=> large', animate('300ms ease-in-out'))
+    ])
+  ]
 })
 export class AppComponent {
   logo = 'https://angular.io/assets/images/logos/angular/shield-large.svg';
@@ -14,12 +26,20 @@ export class AppComponent {
     background: true
   };
   planets = [];
+  state:String = 'small';
 
   constructor(private dataService: DataService) {
   }
 
   loadPlanets() {
     this.planets = this.dataService.planets;
+  }
+
+  ensmall(){
+    this.state = 'small';
+  }
+  enlarge(){
+    this.state = 'large';
   }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {DataService} from "./data.service";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppComponent} from "./app.component";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
